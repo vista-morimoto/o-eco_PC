e37970044892bf97ca0e2941a56cac42d908d91f
vista-morimoto <m-morimoto@vista.co.jp>
2016-11-07 16:47:33 +0900

    ignore second

--- career/results/js/import.js
+++
@@ -1,6 +0,0 @@
-document.write('<script type="text/javascript" src="../../../career/results//js/js/jquery.js"></script>');
-document.write('<script type="text/javascript" src="../../../career/results//js/js/ui.core.js"></script>');
-
-/* scrolltop */
-document.write('<script type="text/javascript" src="../../../career/results//js/js/scrolltop.js"></script>');
-
--- common/js/smartphone.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-	var agent = navigator.userAgent;
-	var redirectPass = '/sample/sp/';
-	if(agent.search(/iPhone/) != -1 || agent.search(/iPod/) != -1 || agent.search(/Android/) != -1){
-		 $('#ForSP').css({display : 'block'});
-	}
-});
